78d45346f2392a9cb58c62508436d72cdf36531e
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-05-31 18:20:58 +0530

    added open and close drop down with div as role button

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,7 +2,27 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dropdown"
 export default class extends Controller {
+
+  //for view/home/index
+  static targets = ["dropdownContent", ,"openButton", "closeButton"]
+
   connect() {
-    console.log("connected to dropdown")
+    this.dropdownContentTarget.hidden = true
+    this.closeButtonTarget.hidden = true
+    // console.log("connected to dropdown")
+  }
+
+  //when click on open drop down button
+  openDropDown(){
+    this.dropdownContentTarget.hidden = false
+    this.closeButtonTarget.hidden = false
+    this.openButtonTarget.hidden = true
+  }
+
+  //when click on close drop down button
+  closeDropDown(){
+    this.closeButtonTarget.hidden = true
+    this.dropdownContentTarget.hidden = true
+    this.openButtonTarget.hidden = false
   }
 }
