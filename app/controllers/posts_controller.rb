0dca90b334c68c625e90bfab9ae1d250afda1d54
Laxmi-Pasi <laxmip.@simformsolutions.com>
2022-05-31 17:08:16 +0530

    craeted home controller with index page and dropdown stimulus controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  layout "post_message"
   # GET /posts or /posts.json
   def index
     @posts = Post.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :posts do
     member do
       post :edit
